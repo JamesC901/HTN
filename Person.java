2aa5c680ae4d976672edf455c2d9d1bea87c2724
Zhang-Sonny <sonnyzhang007@gmail.com>
2021-09-18 13:24:38 -0400

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,16 +1,37 @@
 
+import java.util.ArrayList;
+
 public class Person {
 
 
+  private String uname = "";
+  private String fname = "";
+  private Game games;
+  private String pronouns = "";
+  private int age;
+  ArrayList<Person> friends = new ArrayList<Person>();
+  private String dtag = "";
+
+
   public Person(){
-    String uname = "";
-    String fname = "";
-    Game games;
-    String pronouns = "";
-    int age;
-
-    String dtag = "";
-
-
+    uname = "";
+    fname = "";
+    pronouns = "";
+    age = 0;
+    dtag = "";
   }
+
+  public Person(String uname, String fname, Game games, String pronouns, int age, ArrayList<Person> friends, String dtag) {
+    this.uname = uname;
+    this.fname = fname;
+    this.games = games;
+    this.pronouns = pronouns;
+    this.age = age;
+    this.friends = friends;
+    this.dtag = dtag;
+  }
+
+
+
+
 }
