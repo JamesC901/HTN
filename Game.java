f5a00e631a617efd4dab25044d6358d4c95765b7
Zhang-Sonny <sonnyzhang007@gmail.com>
2021-09-18 13:12:30 -0400

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,12 +1,36 @@
 
 public class Game {
-  public Game(){
+  private String rank;
+  private String gname;
+  private String players;
+
+  public Game() {
     String gname;
     String rank;
     String players;
-    
   }
+    public String getGname() {
+      return gname;
+    }
+    public void setGname(String ngname) {
+      this.gname = ngname;
+    }
+
+    public String getRank() {
+      return rank;
+    }
+    public void setRank(String nrank) {
+      this.rank = nrank;
+    }
+
+    public String getPlayers() {
+      return players;
+    }
+    public void setPlayers(String nplayers){
+      this.players = nplayers;
+    }
+  }
+
   
   
-  
-}
+
